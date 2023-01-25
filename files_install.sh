6d8741a5de2db40ff9252bbbd703505dbf7c2276
felipesuri <felipe@felipesuri.com>
2023-01-25 16:48:10 -0300

    :wrench: Improve my dotfiles

---
+++ files_install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+path=$(pwd)
+
+# Home directory
+ln -sf $path/.gitconfig $HOME/.gitconfig
+ln -sf $path/.zprofile $HOME/.zprofile
+ln -sf $path/.zshenv $HOME/.zshenv
+ln -sf $path/.zshrc $HOME/.zshrc
+ln -sf $path/.hyper.js $HOME/.hyper.js
+
+# Config directory
+[ ! -d $HOME/.config ] && mkdir $HOME/.config
+
+[ -d $HOME/.config/alacritty ] && rm -rf $HOME/.config/alacritty
+ln -sf $path/.config/alacritty $HOME/.config/alacritty
+
+[ -d $HOME/.config/neofetch ] && rm -rf $HOME/.config/neofetch
+ln -sf $path/.config/neofetch $HOME/.config/neofetch
+
+[ -d $HOME/.config/nvim ] && rm -rf $HOME/.config/nvim
+ln -sf $path/.config/nvim $HOME/.config/nvim
+
+[ -d $HOME/.config/lvim ] && rm -rf $HOME/.config/lvim
+ln -sf $path/.config/lvim $HOME/.config/lvim
+
+# Var Directory
+[ ! -d /var ] && sudo mkdir -p /var/spool/cron
+[ ! -d /var/spool ] && sudo mkdir -p /var/spool/cron
+[ -d /var/spool/cron ] && rm -rf /var/spool/cron
+sudo ln -sf $path/cron /var/spool/cron
