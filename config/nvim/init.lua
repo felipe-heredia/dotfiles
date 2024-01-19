a69063a9bdd077fb7c2db20adfbde295a9aa5552
felipe-heredia <felipe@felipesuri.com>
2024-01-19 09:25:41 -0300

    feat(neovim): Change init.lua config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,21 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+require("config.keymaps")
+require("config.options")
+require("config.lazy")
 
-vim.opt.clipboard = "unnamedplus"
-
-require("vim-options")
-require("lazy").setup("plugins")
-
-vim.cmd[[hi Normal ctermbg=NONE guibg=NONE]]
-vim.cmd[[hi NormalNC ctermbg=NONE guibg=NONE]]
-vim.cmd[[hi VertSplit ctermbg=NONE guibg=NONE]]
