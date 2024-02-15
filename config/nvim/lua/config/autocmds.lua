cda419ce956adfadc4088a893603ab60dae6065e
felipe-heredia <felipe@felipesuri.com>
2024-02-15 16:01:46 -0300

    feat(nvim): Adjust plugins

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  command = "FormatWriteLock"
+  command = "FormatWriteLock",
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost *.astro" }, {
+  command = "call system('prettier -w . --plugin=prettier-plugin-astro') | e! | set modifiable",
 })
