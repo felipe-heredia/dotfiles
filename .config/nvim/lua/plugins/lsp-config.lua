f6b0083b6cb82757453f07e442a4014737654596
Felipe Heredia <felipe@felipesuri.com>
2024-03-04 11:52:57 -0300

    feature(neovim): :wrench: Update config (#4)
    * :wrench: Update neovim config
    
    * :wrench: Change git ci and gc commands

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -10,10 +10,10 @@ return {
         log_level = vim.log.levels.WARN,
         filetype = {
           javascript = {
-            require("formatter.filetypes.javascript").prettier,
+            require("formatter.filetypes.javascript").prettierd,
           },
           typescript = {
-            require("formatter.filetypes.typescript").prettier,
+            require("formatter.filetypes.typescript").prettierd,
           },
           lua = {
             require("formatter.filetypes.lua").stylua,
@@ -53,7 +53,7 @@ return {
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
   {
