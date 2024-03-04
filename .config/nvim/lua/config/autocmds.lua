c084cf397e1b6abb6171ae43ec194a5527f2e3c5
felipesuri <felipe.heredia@starcompany.com.br>
2024-03-04 17:51:33 -0300

    refactor: :wrench: Adjust foldexp config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   command = "FormatWriteLock",
 })
+
+local enabled_filetypes = { "typescript", "javascript", "json", "lua" }
+
+vim.api.nvim_exec(
+  string.format(
+    [[
+  autocmd FileType %s setlocal foldmethod=indent
+]],
+    table.concat(enabled_filetypes, ",")
+  ),
+  false
+)
