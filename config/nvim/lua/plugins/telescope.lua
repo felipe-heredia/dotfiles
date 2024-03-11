53ab15743ceed082dbaf1ef89b3a683e5ce7cb19
felipe-heredia <felipe@felipesuri.com>
2024-03-11 16:53:34 -0300

    refactor(neovim): Enhance which-key options

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -5,9 +5,9 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>cs", builtin.colorscheme, {})
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+      vim.keymap.set("n", "<leader>cs", builtin.colorscheme, { desc = "Colorscheme" })
     end,
   },
   {
