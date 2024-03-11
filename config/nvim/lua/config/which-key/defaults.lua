06bdbd17fa65f6b431409aa87fc53f81e624f8a6
felipe-heredia <felipe@felipesuri.com>
2024-03-11 16:53:34 -0300

    refactor(neovim): Enhance which-key options

---
+++ config/nvim/lua/config/which-key/defaults.lua
@@ -0,0 +1,10 @@
+return {
+  mode = { "n", "v" },
+  [";"] = { ":Alpha<CR>", "Dashboard" },
+  w = { ":w!<CR>", "Save" },
+  q = { ":confirm q<CR>", "Quit" },
+  h = { ":nohlsearch<CR>", "No Highlight" },
+  f = {
+    name = "Find",
+  },
+}
