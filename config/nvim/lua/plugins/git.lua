345b70be5a064d0819c3925be3d2ecf36650c937
felipe-heredia <felipe@felipesuri.com>
2024-12-30 10:02:35 -0300

    cleanup(neovim): remove wrong configurations

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -12,8 +12,8 @@ return {
           untracked = { text = "┆" },
         },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-        numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+        numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+        linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
           follow_files = true,
@@ -38,25 +38,15 @@ return {
             text = blame_info.author
           else
             local date_time
-
-            -- if opts.relative_time then
             date_time = require("gitsigns.util").get_relative_time(tonumber(blame_info["author_time"]))
-            -- else
-            --   date_time = os.date("%Y-%m-%d", tonumber(blame_info["author_time"]))
-            -- end
-
             text = string.format("%s, %s - %s", blame_info.author, date_time, blame_info.summary)
           end
           return { { " " .. text, "GitSignsCurrentLineBlame" } }
         end,
-        current_line_blame_formatter_opts = {
-          relative_time = true,
-        },
-
         sign_priority = 6,
         update_debounce = 100,
-        status_formatter = nil, -- Use default
-        max_file_length = 40000, -- Disable if file is longer than this (in lines)
+        status_formatter = nil,
+        max_file_length = 40000,
         preview_config = {
           -- Options passed to nvim_open_win
           border = "single",
