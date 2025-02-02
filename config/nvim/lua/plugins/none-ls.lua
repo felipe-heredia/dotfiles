e149da14c76207cf0442d4bab9ef050e5606ffb5
felipe-heredia <felipe@felipesuri.com>
2025-02-01 22:32:48 -0300

    refactor(nvim): add none-ls extras

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -1,20 +1,20 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+        require("none-ls.diagnostics.eslint_d"),
+
         null_ls.builtins.formatting.stylua,
 
         null_ls.builtins.formatting.prettierd.with({ extra_filetypes = { "astro" } }),
 
-        -- null_ls.builtins.diagnostics.eslint_d,
-        -- null_ls.builtins.formatting.eslint_d,
-
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
-
-        -- null_ls.builtins.formatting.eslint_d.with({ extra_filetypes = { "astro" } }),
       },
     })
   end,
