b96059be39d298ae5116c169af3693c74fe3e297
felipe-heredia <felipe@felipesuri.com>
2025-02-20 13:56:35 -0300

    update tmux and neovim

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -8,12 +8,12 @@ return {
     null_ls.setup({
       sources = {
         require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
 
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd.with({ extra_filetypes = { "astro" } }),
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
-
       },
     })
   end,
